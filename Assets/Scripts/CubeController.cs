5586a3e0435654cdaf810608e2b153755f24e04a
Yamamoto Masao <yamamoto.masa-y@jp.fujitsu.com>
2018-04-12 11:21:31 +0900

    Lesson7課題
    ブロックが地面もしくはブロックが積み重なった時に音を鳴らすようにしています

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+	// キューブの移動速度
+	private float speed = -0.2f;
+
+	// 消滅位置
+	private float deadLine = -10;
+
+	// Use this for initialization
+	void Start(){
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// キューブを移動させる
+		transform.Translate (this.speed, 0, 0);
+
+		// 画面外に出たら破棄する
+		if (transform.position.x < this.deadLine){
+			Destroy (gameObject);
+		}
+	}
+
+	//トリガーモードで他のオブジェクトと接触した場合の処理（追加）
+	void OnTriggerEnter(Collider other) {
+
+		//障害物に衝突した場合（追加）
+		if (other.gameObject.tag == "CubeTag" || other.gameObject.tag == "groudTag") {
+			// 地面に接触したとき／ブロック同士が衝突したときにボリュームを上げる
+			GetComponent<AudioSource> ().volume = 1;
+		} else {
+			if (other.gameObject.tag == "UnityChan2DTag") {
+				// 地面に接触したとき／ブロック同士が衝突したときにボリュームを上げる
+				GetComponent<AudioSource> ().volume = 0;
+			} else {
+				GetComponent<AudioSource> ().volume = 0;
+			}
+		}
+	}
+
+}
